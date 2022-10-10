1bf9e6b48d2eb6bb69ffc58f70457c9f2a6236c6
ShoddyMaintenance <elliott.kalt@gmail.org>
2022-10-09 23:50:03 -0400

    starting find-the-access-codes.py with a correct but slow solution

--- find-the-access-codes.py
+++ find-the-access-codes.py
@@ -1,1 +1,13 @@
 def solution(l):
+    triples = 0
+    for i in range(len(l)):
+        for j in range(i + 1, len(l)):
+            if l[j] % l[i] == 0:
+                for k in range(j + 1, len(l)):
+                    if l[k] % l[j] == 0:
+                        triples += 1
+    return triples
+
+
+assert solution([1, 1, 1]) == 1
+assert solution([1, 2, 3, 4, 5, 6])
