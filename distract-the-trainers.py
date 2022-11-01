c31de052c32a59ab4ce47452d7fddd26bf782573
ShoddyMaintenance <elliott.kalt@gmail.org>
2022-11-01 14:08:21 -0400

    it workssss

--- distract-the-trainers.py
+++ distract-the-trainers.py
@@ -8,11 +8,13 @@ def sortDecroissant(a, b):
 
 def findLoops(list):
     infinites = {}
-    for first in list:
+    for first in range(len(list)):
         infinites[first] = []
-        for second in list:
+        for second in range(len(list)):
+            if first == second:
+                continue
             previous = set()
-            bigger, smaller = first, second
+            bigger, smaller = list[first], list[second]
             while True:
                 bigger, smaller = sortDecroissant(bigger, smaller)
                 if (bigger, smaller) == (0, 0):
@@ -29,14 +31,11 @@ def findLoops(list):
 def solution(banana_list):
     loops = findLoops(banana_list)
     final = len(banana_list)
-    tried = 0
-    while True:
-        if len(loops) - 1 == tried:
-            return final
-        first = sorted(loops.keys(), key=lambda x: len(loops[x]))[tried]
+    while loops:
+        first = sorted(loops.keys(), key=lambda x: len(loops[x]))[0]
         loops[first] = sorted(loops[first], key=lambda x: len(loops[x]))
-        if len(loops[first]) == 0 and tried < len(loops):
-            tried += 1
+        if len(loops[first]) == 0:
+            del loops[first]
             continue
         final -= 2
         second = loops[first][0]
@@ -44,10 +43,9 @@ def solution(banana_list):
             loops[i].remove(first)
         for i in loops[second]:
             loops[i].remove(second)
-        loops[second] = []
-        loops[first] = []
-        if not any(a != [] for a in loops.values()):
-            return final
+        del loops[second]
+        del loops[first]
+    return final
 
 
-print(solution([1, 7, 3, 21, 13, 19]))
+print(solution([1]))
