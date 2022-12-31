376284cd938522e12557a430b4d86af0bd0eb569
syranis <elliott.kalt@gmail.com>
2022-12-30 23:45:44 -0500

    negative cycle detection

--- running-with-bunnies.py
+++ running-with-bunnies.py
@@ -2,43 +2,42 @@ import pprint
 
 
 def solution(times, time_limit):
-    return allShortest(times)[0]
-
-
-def shortestPath(times, origin):
-    """
-    Single source Bellman-Ford algorithm.
-    Finds the shortest path to all other nodes from an origin node and their predecessor.
-    """
-    # Standard Dijkstra's initialization
-    length = len(times)
-    cost_estimates = [float('inf')] * length
-    previous = [None] * length
-    queue = list(range(length))
-    cost_estimates[origin] = 0
-
-    # Bellman-Ford relaxation
-    for i in range(length - 1):
-        for u in range(length):
-            for v in range(length):
-                if cost_estimates[u] + times[u][v] < cost_estimates[v]:
-                    cost_estimates[v] = cost_estimates[u] + times[u][v]
-                    previous[v] = u
-    return cost_estimates, previous
-
-
-def allShortest(times):
-    # Get shortest
     length = len(times)
     previous = [[None] * length] * length
     shortest_times = [[0] * length] * length
     for i in range(length):
-        shortest_times[i], previous[i] = shortestPath(times, i)
+        try:
+            shortest_times[i], previous[i] = shortestSingleSource(times, i)
+        except TypeError:
+            return list(range(length - 2))
     return shortest_times, previous
+
+
+def shortestSingleSource(times, origin):
+    # initialization
+    length = len(times)
+    cost_estimates = [float('inf')] * length
+    previous = [None] * length
+    cost_estimates[origin] = 0
+
+    # relaxation
+    for u in range(length):
+        for v in range(length):
+            if cost_estimates[u] + times[u][v] < cost_estimates[v]:
+                cost_estimates[v] = cost_estimates[u] + times[u][v]
+                previous[v] = u
+
+    # negative cycle detection
+    for u in range(length):
+        for v in range(length):
+            if cost_estimates[u] + times[u][v] < cost_estimates[v]:
+                return True
+
+    return cost_estimates, previous
 
 
 pprint.pprint(solution([[0, 2, 2, 2, -1],
                         [9, 0, 2, 2, -1],
                         [9, 3, 0, 2, -1],
                         [9, 3, 2, 0, -1],
-                        [9, 3, 2, 2, 0]], 0))
+                        [9, 3, -1, 2, 0]], 0))
