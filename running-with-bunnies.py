88588ce1278f18a5f503aed9326732037c34451c
syranis <elliott.kalt@gmail.com>
2022-12-31 00:26:20 -0500

    backtrack

--- running-with-bunnies.py
+++ running-with-bunnies.py
@@ -2,15 +2,17 @@ import pprint
 
 
 def solution(times, time_limit):
+    # find the shortest path from each node to all other nodes (and check for negative cycles)
     length = len(times)
     previous = [[None] * length] * length
     shortest_times = [[0] * length] * length
     for i in range(length):
+        # TODO: Find a better way to do this
         try:
             shortest_times[i], previous[i] = shortestSingleSource(times, i)
         except TypeError:
             return list(range(length - 2))
-    return shortest_times, previous
+    return
 
 
 def shortestSingleSource(times, origin):
@@ -36,8 +38,17 @@ def shortestSingleSource(times, origin):
     return cost_estimates, previous
 
 
+def backtrack(origin, destination, previous):
+    visited = {origin, destination}
+    current = previous[destination][origin]
+    while current and current != origin:
+        visited.add(current)
+        current = previous[destination][current]
+    return visited
+
+
 pprint.pprint(solution([[0, 2, 2, 2, -1],
                         [9, 0, 2, 2, -1],
                         [9, 3, 0, 2, -1],
                         [9, 3, 2, 0, -1],
-                        [9, 3, -1, 2, 0]], 0))
+                        [9, 3, 2, 2, 0]], 0))
