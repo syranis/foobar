a946ec20514173237e0763d8ac6cecfdaa746b80
syranis <elliott.kalt@gmail.com>
2023-01-01 22:29:06 -0500

    forgot to relax length - 1 times, not just once.  Only one test case left!

--- running-with-bunnies.py
+++ running-with-bunnies.py
@@ -2,11 +2,14 @@ import itertools
 
 
 def solution(times, time_limit):
-    # find the shortest path from each node to all other nodes (and check for negative cycles)
+    # initialize
     length = len(times)
+    previous = [[None] * length] * length
     shortest_times = [[0] * length] * length
+
+    # find the shortest path from each node to all other nodes (and check the graph for negative cycles)
     for i in range(length):
-        shortest_times[i] = shortestSingleSource(times, i)
+        shortest_times[i], previous[i] = shortestSingleSource(times, i)
         if cycleDetection(times, shortest_times[i]):
             return list(range(length - 2))
 
@@ -22,16 +25,15 @@ def solution(times, time_limit):
             for edge_origin in range(len(combo) - 1):
                 path_length += shortest_times[combo[edge_origin]][combo[edge_origin + 1]]
 
-            if path_length > time_limit or len(combo) <= len(best_try):
-                continue
-            best_try = combo
+            if path_length <= time_limit and len(combo) > len(best_try):
+                best_try = combo
 
     # bunnies only
-    answer = []
-    for i in range(1, len(best_try) - 1):
-        answer.append(best_try[i] - 1)
+    answer = set()
+    for edge_origin in range(len(best_try) - 1):
+        answer.update(backtrack(best_try[edge_origin], best_try[edge_origin + 1], previous))
 
-    return sorted(answer)
+    return [x - 1 for x in sorted(list(answer))[1:-1]]
 
 
 def powerset(num_bunnies):
@@ -43,14 +45,17 @@ def shortestSingleSource(times, origin):
     # initialization
     length = len(times)
     cost_estimates = [float('inf')] * length
+    previous = [None] * length
     cost_estimates[origin] = 0
 
     # relaxation
-    for u in range(length):
-        for v in range(length):
-            if cost_estimates[u] + times[u][v] < cost_estimates[v]:
-                cost_estimates[v] = cost_estimates[u] + times[u][v]
-    return cost_estimates
+    for i in range(length - 1):
+        for u in range(length):
+            for v in range(length):
+                if cost_estimates[u] + times[u][v] < cost_estimates[v]:
+                    cost_estimates[v] = cost_estimates[u] + times[u][v]
+                    previous[v] = u
+    return cost_estimates, previous
 
 
 def cycleDetection(times, time_slice):
@@ -62,8 +67,10 @@ def cycleDetection(times, time_slice):
     return False
 
 
-print(solution([[0, 2, 2, 2, -1],
-                [9, 0, 2, 2, -1],
-                [9, 3, 0, 2, -1],
-                [9, 3, 2, 0, -1],
-                [9, 3, 2, 2, 0]], 1))
+def backtrack(origin, destination, previous):
+    visited = {origin, destination}
+    current = previous[destination][origin]
+    while current and current != origin:
+        visited.add(current)
+        current = previous[destination][current]
+    return visited
